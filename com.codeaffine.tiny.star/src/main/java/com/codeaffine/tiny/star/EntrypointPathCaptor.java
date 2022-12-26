d29fd0d078f1416a1a8c27b8e37513d721cba7dc
Frank Appel <fappel@codeaffine.com>
2022-12-26 06:03:04 +0100

    reduce boiler-plate

--- com.codeaffine.tiny.star/src/main/java/com/codeaffine/tiny/star/EntrypointPathCaptor.java
+++ com.codeaffine.tiny.star/src/main/java/com/codeaffine/tiny/star/EntrypointPathCaptor.java
@@ -1,24 +1,18 @@
 package com.codeaffine.tiny.star;
 
-import static lombok.AccessLevel.PACKAGE;
-import static lombok.AccessLevel.PRIVATE;
-
-import org.eclipse.rap.rwt.application.Application;
-import org.eclipse.rap.rwt.application.ApplicationConfiguration;
-import org.eclipse.rap.rwt.application.EntryPoint;
-import org.eclipse.rap.rwt.application.EntryPointFactory;
-import org.eclipse.rap.rwt.application.ExceptionHandler;
-import org.eclipse.rap.rwt.service.ResourceLoader;
-import org.eclipse.rap.rwt.service.ServiceHandler;
-import org.eclipse.rap.rwt.service.SettingStoreFactory;
-import org.eclipse.swt.widgets.Widget;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Delegate;
+import org.eclipse.rap.rwt.application.*;
+import org.eclipse.rap.rwt.internal.application.ApplicationImpl;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public class EntrypointPathCaptor {
@@ -28,10 +22,14 @@ public class EntrypointPathCaptor {
 
         @NonNull
         private final Set<String> paths;
+        @Delegate(excludes = Excludes.class)
+        private final Application application = new ApplicationImpl(null,  null);
 
-        @Override
-        public void setOperationMode(OperationMode operationMode) {
-            // do nothing
+        interface Excludes {
+            @SuppressWarnings("unused")
+            void addEntryPoint(String path, Class<? extends EntryPoint> entryPointType, Map<String, String> properties);
+            @SuppressWarnings("unused")
+            void addEntryPoint(String path, EntryPointFactory entryPointFactory, Map<String, String> properties);
         }
 
         @Override
@@ -42,46 +40,6 @@ public class EntrypointPathCaptor {
         @Override
         public void addEntryPoint(String path, EntryPointFactory entryPointFactory, Map<String, String> properties) {
             paths.add(path);
-        }
-
-        @Override
-        public void addStyleSheet(String themeId, String styleSheetLocation) {
-            // do nothing
-        }
-
-        @Override
-        public void addStyleSheet(String themeId, String styleSheetLocation, ResourceLoader resourceLoader) {
-            // do nothing
-        }
-
-        @Override
-        public void setAttribute(String name, Object value) {
-            // do nothing
-        }
-
-        @Override
-        public void setSettingStoreFactory(SettingStoreFactory settingStoreFactory) {
-            // do nothing
-        }
-
-        @Override
-        public void setExceptionHandler(ExceptionHandler exceptionHandler) {
-            // do nothing
-        }
-
-        @Override
-        public void addThemeableWidget(Class<? extends Widget> widget) {
-            // do nothing
-        }
-
-        @Override
-        public void addServiceHandler(String serviceHandlerId, ServiceHandler serviceHandler) {
-            // do nothing
-        }
-
-        @Override
-        public void addResource(String resourceName, ResourceLoader resourceLoader) {
-            // do nothing
         }
     }
 

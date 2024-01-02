cc7a7cee3965ffae92c4c1e944a86bdcf4e5b707
Frank Appel <fappel@codeaffine.com>
2024-01-02 13:26:05 +0100

    Update licence text

--- com.codeaffine.tiny.star/src/test/java/com/codeaffine/tiny/star/spi/SecureSocketLayerConfigurationTest.java
+++ com.codeaffine.tiny.star/src/test/java/com/codeaffine/tiny/star/spi/SecureSocketLayerConfigurationTest.java
@@ -1,5 +1,5 @@
 /**
- * <p>Copyright (c) 2022-2023 CA Code Affine GmbH (<a href="https://codeaffine.com">codeaffine.com</a>)</p>
+ * <p>Copyright (c) 2022-2024 CA Code Affine GmbH (<a href="https://codeaffine.com">codeaffine.com</a>)</p>
  * <p>All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
